8f953c7fb5e1f9c957f14486e2c5042876035b64
Haripriya1303 <haripriyavg9@gmail.com>
2026-04-25 11:45:29 +0530

    added web app

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name='HealthAssist-AI',
+    version='0.1.0',
+    author='Haripriya',
+    author_email='haripriyavg9@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
